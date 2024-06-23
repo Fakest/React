04c477292f932219a1ae60a1a21963c7bce63615
Fakest <hjbeggs99@gmail.com>
2024-06-23 22:03:42 +0100

    React tutorial first commit

---
+++ react-app/src/App.tsx
@@ -0,0 +1,14 @@
+import ListGroup from './components/ListGroup'
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <div>
+        <ListGroup></ListGroup>
+      </div>
+    </>
+  )
+}
+
+export default App
