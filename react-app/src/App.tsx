8b185c7b69b17394d0bc9ca24f1196e61d2baf0d
Fakest <hjbeggs99@gmail.com>
2024-06-23 23:20:29 +0100

    State, props, imbedded functionality

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,14 +1,27 @@
-import ListGroup from './components/ListGroup'
-import './App.css'
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+
 function App() {
+  let items = ["Tokyo", "Kanazawa", "Kyoto", "Hiroshima", "Osaka"];
+  const handleSelectItem = (item: String) => {
+    console.log(item);
+  };
 
   return (
     <>
       <div>
-        <ListGroup></ListGroup>
+        <ListGroup
+          items={items}
+          heading={"Cities I have been to!"}
+          onSelectItem={handleSelectItem}
+        ></ListGroup>
+
+        <Alert>
+          Hello, <span>World!</span>
+        </Alert>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
